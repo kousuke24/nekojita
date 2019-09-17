b42e05eb1307152bc747aa40bab6e3fcc1603449
kousuke24 <kousuke_zuka24@icloud.com>
2019-09-17 19:18:21 +0900

    update user edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+    controllers: { registrations: 'registrations' }
+
   get 'tops/index'
   root to: 'tops#index'
+
+  get '/users/:id', to: 'users#show', as: 'user'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
