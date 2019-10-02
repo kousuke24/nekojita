4ca5aea0618657f8a8b1bd98b67b1a964746d65b
kousuke24 <kousuke_zuka24@icloud.com>
2019-10-02 22:25:01 +0900

    aws s3 update

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,8 +8,8 @@ CarrierWave.configure do |config|
   config.fog_directory = 'nekojita'
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
     region: ENV['AWS_DEFAULT_REGION'],
     path_style: true
   }
