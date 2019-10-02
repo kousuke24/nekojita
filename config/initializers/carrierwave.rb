064a43775149bca15d9796a76d11d9c00b05c65e
kousuke24 <kousuke_zuka24@icloud.com>
2019-10-03 03:38:07 +0900

    precompiling assets failed cure

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,6 +2,7 @@ equire 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
+if Rails.env.production?
   CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
